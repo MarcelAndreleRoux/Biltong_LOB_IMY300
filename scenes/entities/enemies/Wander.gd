2f8a7100c049a31915e3db580266108dd89032cd
MarcelAndreLeRoux <u22598805@tuks.co.za>
2024-07-19 06:21:16 +0200

    Enemy movement fix

--- scenes/entities/enemies/Wander.gd
+++ scenes/entities/enemies/Wander.gd
@@ -2,50 +2,54 @@ extends Node2D
 
 @export var group_name: String
 
-@onready var wander_at_place_time = $WanderAtPlaceTime
-
 var positions: Array
-var temp_positions: Array
-var current_position: Marker2D
-
+var current_position_index: int = 0
 var direction: Vector2 = Vector2.ZERO
 var waiting: bool = false
 
 func _ready():
 	positions = get_tree().get_nodes_in_group(group_name)
-	_get_positions()
-	_get_next_position()
+	if positions.size() > 0:
+		_get_next_position()
+	else:
+		print("No positions found in group")
 
 func _physics_process(delta):
 	if waiting:
 		return
 
-	if global_position.distance_to(current_position.position) < 10:
+	if global_position.distance_to(positions[current_position_index].global_position) < 10:
 		_stop_and_wait()
 	else:
 		move_towards_position(delta)
 
-func _get_positions():
-	temp_positions = positions.duplicate()
-	temp_positions.shuffle()
-
 func _get_next_position():
-	if temp_positions.is_empty():
-		_get_positions()
-	current_position = temp_positions.pop_front()
-	direction = (current_position.position - global_position).normalized()
+	current_position_index = (current_position_index + 1) % positions.size()
+	direction = (positions[current_position_index].global_position - global_position).normalized()
 
 func _stop_and_wait():
 	waiting = true
-	wander_at_place_time.start()
+	_create_timer()
+
+func _create_timer():
+	var timer = Timer.new()
+	timer.wait_time = 3.0  # Set your desired wait time here
+	timer.one_shot = true
+	timer.timeout.connect(_on_wander_at_place_time_timeout)
+	add_child(timer)
+	timer.start()
 
 func move_towards_position(delta):
-	var move_amount = direction * delta
-	if global_position.distance_to(current_position.position) <= move_amount.length():
-		global_position = current_position.position
+	var move_amount = direction * delta  # Ensure speed is factored in
+	if global_position.distance_to(positions[current_position_index].global_position) <= move_amount.length():
+		global_position = positions[current_position_index].global_position
+		_stop_and_wait()  # Stop and wait when reaching the marker
 	else:
 		global_position += move_amount
 
 func _on_wander_at_place_time_timeout():
 	waiting = false
 	_get_next_position()
+	var timer = get_node("Timer")
+	if timer != null:
+		timer.queue_free()  # Remove the timer node
