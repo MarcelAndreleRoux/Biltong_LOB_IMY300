11ee369337fa170758e7fef90211248e118d5aa5
MarcelAndreLeRoux <u22598805@tuks.co.za>
2024-08-12 01:39:52 +0200

    Basic Changes and added new sprites

---
+++ scenes/UI/inventory.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var food = $ItemOne
+@onready var fire = $ItemTwo
+@onready var water = $ItemThree
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
