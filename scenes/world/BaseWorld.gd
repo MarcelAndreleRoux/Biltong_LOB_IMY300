eb393b33674045a365886f6478d2ca84eaa5e1a1
Stefan Tolken <100857488+MeatyDotPng@users.noreply.github.com>
2024-09-25 22:32:10 +0200

    Update level 3 and implement level 4

--- scenes/world/BaseWorld.gd
+++ scenes/world/BaseWorld.gd
@@ -62,12 +62,13 @@ func _ready():
 
 	player_raycast.enabled = true
 	GlobalValues.turtle_original_pos = turtle.global_position
-
+	
 	# Add all vines to the player's raycast exceptions
 	if GlobalValues.vinesSize == "small":
 		for vine in get_tree().get_nodes_in_group("vines"):
 			player_raycast.add_exception(vine)
 		
+	enemy_raycast.add_exception(hedgehog)
 	turtle_raycast.add_exception(hedgehog)
 	turtle_raycast.add_exception(electric_lizard)
 
