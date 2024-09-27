366817c6914a6dda4c21d966c312a05835c05b4b
Stefan Tolken <100857488+MeatyDotPng@users.noreply.github.com>
2024-09-27 21:20:59 +0200

    Created win screen

---
+++ scenes/UI/win.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+@onready var confetti = $Confetti
+@onready var select = $select
+
+var exit: bool = false
+var reset: bool = false
+
+func _ready():
+	self.hide()
+
+func _on_exit_pressed():
+	exit = true
+	select.play()
+
+func _on_select_finished():
+	if exit:
+		get_tree().paused = false
+		get_tree().change_scene_to_file("res://scenes/UI/menu.tscn")
+		exit = false
+	elif reset:
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+		reset = false
+
+func death_lose():
+	get_tree().paused = true
+	self.show()
+	confetti.play()
