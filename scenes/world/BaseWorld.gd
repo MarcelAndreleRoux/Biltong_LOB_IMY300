e66f18270f59895612aa92c979b66cfd3e738a5c
Stefan Tolken <100857488+MeatyDotPng@users.noreply.github.com>
2024-10-28 22:08:24 +0200

    Added popup

--- scenes/world/BaseWorld.gd
+++ scenes/world/BaseWorld.gd
@@ -17,7 +17,7 @@ class_name BaseWorld
 @onready var shake_camera = $ShakeCamera
 
 # Manager
-@onready var popup_manager = $PopupManager
+@onready var popup_manager = $PopupCanvasLayer/PopupManager
 
 # Raycasts
 @onready var turtle_raycast = $Turtle/RayCast2D
@@ -78,7 +78,7 @@ const HELPFUL_MESSAGES = {
 }
 
 const WARNING_MESSAGES = {
-	"hazmat": "Cannot enter test cambers without a hazmat suit",
+	"hazmat": "Cannot enter cambers without a hazmat suit",
 	"new_lizard": "You have spotted a lizard, throw it with water"
 }
 
